8ee31103f1c7bc0b0b4066bb495101fe116a711e
Jasielprogramador <asierrosa@gmail.com>
2022-01-06 14:45:29 +0100

    -todo acabado

---
+++ scripts/mongodb/delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/asier/PycharmProjects/grupal_as/scripts/mongodb/lua
+
+sysbench  oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=db --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto cleanup
+
+sysbench oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=db --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto prepare
+
+sysbench  oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=sbtest --mongodb-host=localhost --mongodb-port=27017 --time=5  --point_selects=1 --simple_ranges=0 --sum_ranges=0 --order_ranges=0 --distinct_ranges=0 --index_updates=0 --non_index_updates=0  --delete_inserts=0  --report-interval=1 --rand-type=pareto run
--- scripts/mongodb/insert.sh
+++ scripts/mongodb/insert.sh
@@ -2,8 +2,8 @@
 
 cd /home/asier/PycharmProjects/grupal_as/scripts/mongodb/lua
 
-sysbench  oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=sbtest --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto cleanup
+sysbench  oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=db --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto cleanup
 
-sysbench oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=sbtest --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto prepare
+sysbench oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=db --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto prepare
 
-sysbench  oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=sbtest --mongodb-host=localhost --mongodb-port=27017 --time=120 --report-interval=1 --rand-type=pareto run
+sysbench oltp-mongo.lua --tables=10 --threads=10 --table-size=100 --mongodb-db=db --mongodb-host=localhost --read-only=on --mongodb-port=27017 --time=10 --forced-shutdown=10 --report-interval=1 --rand-type=pareto run
